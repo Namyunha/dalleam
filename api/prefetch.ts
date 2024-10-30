082a8e8e2e86a269f79dba706bc87f86c9f21fc7
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-10-30 19:18:21 +0900

    feat: 모임 prefetch 쿼리 추가

--- api/prefetch.ts
+++ api/prefetch.ts
@@ -2,6 +2,10 @@ import { getReviews, getScores } from './review';
 import { QueryClient } from '@tanstack/react-query';
 import { GatheringType, LocationType, SortType } from '@/types/gathering';
 import { reviewQueryKeys, reviewScoresQueryKeys } from '@/types/review';
+import { gatheringQueryKeys } from '@/types/gathering';
+import { getGatherings } from './gathering';
+
+const queryClient = new QueryClient();
 
 export const useReviewPrefetchQuery = async () => {
   let queryKeys = {
@@ -12,7 +16,6 @@ export const useReviewPrefetchQuery = async () => {
   };
   let reviewQueryKeys: reviewQueryKeys = [['reviews'], queryKeys];
   let reviewScoresQueryKeys: reviewScoresQueryKeys = [['reviews', 'scores'], queryKeys];
-  const queryClient = new QueryClient();
   await Promise.all([
     queryClient.prefetchInfiniteQuery({
       queryKey: reviewQueryKeys,
@@ -24,6 +27,21 @@ export const useReviewPrefetchQuery = async () => {
       queryFn: () => getScores(),
     }),
   ]);
+  return queryClient;
+};
 
+export const gatheringPrefetchQuery = async () => {
+  let queryKeys = {
+    type: 'DALLAEMFIT' as GatheringType,
+    location: '지역 선택' as LocationType,
+    date: '날짜 선택',
+    sortBy: '마감 임박' as SortType,
+  };
+  let gatheringQueryKeys: gatheringQueryKeys = [['gathering'], queryKeys];
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: gatheringQueryKeys,
+    queryFn: ({ pageParam }) => getGatherings({ pageParam }),
+    initialPageParam: 0,
+  });
   return queryClient;
 };
