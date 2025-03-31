9217d772effc53b66f12f09398bd977ac3020e7c
Namyunha <108773845+Namyunha@users.noreply.github.com>
2025-03-31 20:03:59 +0900

    design: button story

---
+++ stories/EmptyButton.stories.ts
@@ -0,0 +1,114 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import Button from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Example/EmptyButton',
+  component: Button,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Gray: Story = {
+  args: {
+    variant: 'gray',
+    size: 'lg',
+    fillState: 'empty',
+    children: 'gray',
+  },
+};
+
+export const GraySmall: Story = {
+  args: {
+    variant: 'gray',
+    size: 'sm',
+    fillState: 'empty',
+    children: 'gray',
+  },
+};
+
+export const Orange: Story = {
+  args: {
+    variant: 'orange',
+    size: 'lg',
+    fillState: 'empty',
+    children: 'orange',
+  },
+};
+
+export const OrangeSmall: Story = {
+  args: {
+    variant: 'orange',
+    size: 'sm',
+    fillState: 'empty',
+    children: 'orange',
+  },
+};
+
+export const Tomato: Story = {
+  args: {
+    variant: 'tomato',
+    size: 'lg',
+    fillState: 'empty',
+    children: 'tomato',
+  },
+};
+
+export const TomatoSmall: Story = {
+  args: {
+    variant: 'tomato',
+    size: 'sm',
+    fillState: 'empty',
+    children: 'tomato',
+  },
+};
+
+export const Red: Story = {
+  args: {
+    variant: 'red',
+    size: 'lg',
+    fillState: 'empty',
+    children: 'tomato',
+  },
+};
+
+export const RedSmall: Story = {
+  args: {
+    variant: 'red',
+    size: 'sm',
+    fillState: 'empty',
+    children: 'tomato',
+  },
+};
+
+export const Invalidate: Story = {
+  args: {
+    variant: 'invalidate',
+    size: 'lg',
+    fillState: 'empty',
+    children: 'invalidate',
+  },
+};
+
+export const InvalidateSmall: Story = {
+  args: {
+    variant: 'invalidate',
+    size: 'sm',
+    fillState: 'empty',
+    children: 'invalidate',
+  },
+};
